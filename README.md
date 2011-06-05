48bee057ffd7179fb23c667fc68c4c974369dd96
Marius Butuc <marius.butuc@gmail.com>
2011-06-04 22:05:27 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#  The first application
+
+This is an alteration of the [Railstutorial's first application](http://ruby.railstutorial.org/ruby-on-rails-tutorial-book#sec:the_first_application) that has been ported to Rails 3.1 + haml.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstAppH31::Application.config.secret_token = 'dda0cb2231624c58b8796613a269af62cef8272c43fa6d07a38edb0723af77f148797a1e0f4f7f79e219db23e37242f357177fff25e89b76524b23dee0a6f4df'
